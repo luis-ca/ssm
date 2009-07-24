29a986ea23d0a3942fa616b2d7a97480a3025834
Danilo Sato <dtsato@gmail.com>
2009-07-24 18:02:43 +0100

    Adding (broken) features for storing state as string

--- features/ActiveRecord/step_definitions/active_record_steps.rb
+++ features/ActiveRecord/step_definitions/active_record_steps.rb
@@ -1,7 +1,55 @@
-Given "I create an ActiveRecord based state machine" do
-  Given "I have declared a new ActiveRecord model Door"
-  Given "I have added an initial state named closed"
-  Given "I have added a state called opened"
-  Given "I have added an event named open_it"
-  Given "I have added an index based property I will use to manage persistence with ActiveRecord"
+require 'active_record'
+require 'sqlite3'
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database  => "tmp_sqlite_file"
+)
+
+ActiveRecord::Migration::create_table :doors do |t|
+  t.integer :state_idx
+  t.string  :state_str
+end
+
+at_exit do
+  File.delete("tmp_sqlite_file")
+end
+
+Given /^I have declared a new ActiveRecord model Door$/ do
+  class Door < ActiveRecord::Base
+    include SSM
+  end
+end
+
+Given /^I have added an index based property I will use to manage persistence with ActiveRecord$/ do
+  class Door < ActiveRecord::Base
+    ssm_inject_state_into :state_idx, :as_integer => true, :strategy => :active_record
+  end
+end
+
+Given /^I have added a string based property I will use to manage persistence with ActiveRecord$/ do
+  class Door < ActiveRecord::Base
+    ssm_inject_state_into :state_str, :strategy => :active_record
+  end
+end
+
+And /^I save the instance of door$/ do
+  @door.save
+end
+
+When /^I load the instance of Door$/ do
+  @door = Door.find(@door.id)
+end
+
+Then /^the instance of Door should save$/ do
+  @door.save
+end
+
+Then /^the state should persist when reloaded$/ do
+  @door.reload
+  @door.is?(:opened).should be_true
+end
+
+Then /^the state should persist when loading from scratch$/ do
+  @door.is?(:opened).should be_true
 end
--- features/Object/step_definitions/object_steps.rb
+++ features/Object/step_definitions/object_steps.rb
@@ -1,6 +1,33 @@
-Given "I create a simple state machine" do
-  Given "I have declared a new Door class"
-  And "I have added an initial state named closed"
-  And "I have added a state called opened"
-  And "I have added an event named open_it"
+Given /^I have declared a new Door class$/ do
+  class Door
+    include SSM
+  end
+end
+
+Given /^I have added an initial state named closed$/ do
+  class Door
+    ssm_initial_state :closed
+  end
+end
+
+Given /^I have added a state called opened$/ do
+  class Door
+    ssm_state :opened
+  end
+end
+
+Given /^I have added an event named open_it$/ do
+  class Door
+    ssm_event :open_it, :from => [:closed], :to => :opened do
+    end
+  end
+end
+
+When /^I trigger the open event on an instance of Door$/ do
+  @door = Door.new
+  @door.open_it
+end
+
+Then /^the state of the instance of Door should be opened$/ do
+  @door.is?(:opened)
 end
